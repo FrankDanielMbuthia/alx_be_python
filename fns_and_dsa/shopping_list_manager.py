dcd8307a7353072832e2d19ec2f3f605736e45eb
Frank Daniel <mbuthiawanganju@gmail.com>
2025-07-27 15:15:54 +0300

    Updated the shopping list manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -9,16 +9,16 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Enter your choice: ")
-        if choice == "1":
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
             item = input("Enter an item: ")
             shopping_list.append(item)
-        elif choice == "2":
+        elif choice == 2:
             item = input("Enter item to be removed: ")
             shopping_list.remove(item)
-        elif choice == "3":
+        elif choice == 3:
             print(shopping_list)
-        elif choice == "4":
+        elif choice == 4:
             print("Goodbye!")
             break
         else:
